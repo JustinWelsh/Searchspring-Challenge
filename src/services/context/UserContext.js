fcb777dccc489363f74c6484a31c3af3306b1495
Justin Welsh <justinwelsh33@gmail.com>
2023-11-10 00:35:48 -0600

    update default search

--- src/services/context/UserContext.js
+++ src/services/context/UserContext.js
@@ -3,7 +3,7 @@ import React, { createContext, useState, useContext } from "react";
 const UserContext = createContext({});
 
 const UserContextProvider = ({ children }) => {
-  const [userSearch, setUserSearch] = useState("");
+  const [userSearch, setUserSearch] = useState("winter");
   const [cart, setCart] = useState(0);
   const [products, setProducts] = useState([]);
   const [pagination, setPagination] = useState({});
