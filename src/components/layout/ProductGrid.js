eed1ddccb122ff49ee73b0cb7a729a236ed123d1
Justin Welsh <justinwelsh33@gmail.com>
2023-11-09 20:25:23 -0600

    Updated key

--- src/components/layout/ProductGrid.js
+++ src/components/layout/ProductGrid.js
@@ -23,72 +23,56 @@ export const ProductGrid = ({ results, setCart }) => {
   return (
     <div className="py-5 px-10 gap-10 justify-center grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 2xl:grid-cols-5">
       {results.map((product, index) => (
-        <>
-          <Card
-            className=""
-            shadow="sm"
-            key={index}
-            isPressable
-            onPress={() => console.log("product pressed")}
-          >
-            <CardBody className="overflow-visible p-0">
-              <div className="py-5 px-20 absolute z-30 top-5 -left-[55px] -rotate-45 bg-red-500 opacity-80"></div>
+        <Card
+          key={product.id}
+          className=""
+          shadow="sm"
+          isPressable
+          onPress={() => console.log("product pressed")}
+        >
+          <CardBody className="overflow-visible p-0">
+            {product.on_sale[0] === "Yes" && (
+              <>
+                <div className="py-5 px-20 absolute z-30 top-5 -left-[55px] -rotate-45 bg-red-500 opacity-80"></div>
 
-              <span className="p-2 px-12 text-white absolute z-30 top-3 -left-12 -rotate-45 font-bold">
-                {getDiscount(product.msrp, product.price)}% Off
-              </span>
-              <Image
-                isZoomed
-                shadow="sm"
-                radius="lg"
-                width="100"
-                alt={product.title}
-                className="w-full object-cover max-h-[500px]"
-                src={product.thumbnailImageUrl}
-                fallbackSrc={`${process.env.PUBLIC_URL}/images/test3.jpg`}
-              />
-            </CardBody>
-            <CardFooter className="block text-small justify-between">
-              <b>{product.title}</b>
-              <div className="flex justify-between mt-4">
-                <div className="flex">
-                  <p className="text-green-500 text-2xl textShadow font-semibold">
-                    ${product.price}
-                  </p>
-                  {product.msrp > product.price && (
-                    <p className="text-slate-300 line-through">
-                      ${product.msrp}
-                    </p>
-                  )}
-                </div>
-                <Button
-                  className="bg-gradient-to-r from-amber-200 to-amber-300"
-                  size="sm"
-                  onClick={handleAddToCart}
-                >
-                  <FontAwesomeIcon icon={faBagShopping} className="text-xl" />
-                </Button>
+                <span className="p-2 px-12 text-white absolute z-30 top-3 -left-12 -rotate-45 font-bold">
+                  {/* {getDiscount(product.msrp, product.price)}% Off */}
+                  On Sale!
+                </span>
+              </>
+            )}
+            <Image
+              isZoomed
+              shadow="sm"
+              radius="lg"
+              width="100"
+              alt={product.title}
+              className="w-full object-cover max-h-[500px]"
+              src={product.thumbnailImageUrl}
+              fallbackSrc={`${process.env.PUBLIC_URL}/images/test3.jpg`}
+            />
+          </CardBody>
+          <CardFooter className="block text-small justify-between">
+            <b>{product.title}</b>
+            <div className="flex justify-between mt-4">
+              <div className="flex">
+                <p className="text-green-500 text-2xl textShadow font-semibold">
+                  ${product.price}
+                </p>
+                {product.msrp > product.price && (
+                  <p className="text-slate-300 line-through">${product.msrp}</p>
+                )}
               </div>
-            </CardFooter>
-          </Card>
-
-          {/* <Card className="py-4">
-            <CardHeader className="pb-0 pt-2 px-4 flex-col products-start">
-              <p className="text-tiny uppercase font-bold">Daily Mix</p>
-              <small className="text-default-500">12 Tracks</small>
-              <h4 className="font-bold text-large">Frontend Radio</h4>
-            </CardHeader>
-            <CardBody className="overflow-visible py-2">
-              <Image
-                isZoomed
-                alt={product.title}
-                className="object-cover rounded-xl"
-                src={product.thumbnailImageUrl}
-                width={270}
-              />
-            </CardBody>
-          </Card> */}
-        </>
+              <Button
+                className="bg-gradient-to-r from-amber-200 to-amber-300"
+                size="sm"
+                onClick={handleAddToCart}
+              >
+                <FontAwesomeIcon icon={faBagShopping} className="text-xl" />
+              </Button>
+            </div>
+          </CardFooter>
+        </Card>
       ))}
     </div>
   );
