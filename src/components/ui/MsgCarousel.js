77feedcc8687fec9a535a1c93faea44a55207f0f
Justin Welsh <justinwelsh33@gmail.com>
2024-01-29 23:02:46 -0600

    Added MsgCarousel component

---
+++ src/components/ui/MsgCarousel.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+
+const MsgCarousel = ({ texts, interval = 5000 }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % texts.length);
+    }, interval);
+
+    return () => clearInterval(intervalId);
+  }, [currentIndex, interval, texts.length]);
+
+  return <div className="msg-carousel">{texts[currentIndex]}</div>;
+};
+
+export default MsgCarousel;
